3c885f180b3c888007c6827044fd89b436734114
yassine said <yassine.said@esprit.tn>
2019-10-22 18:54:51 +0100

    Added User & Etudiant

---
+++ 4twin3-osp-pfe-ejb/src/main/java/tn/esprit/pfe/entities/User.java
@@ -0,0 +1,89 @@
+package tn.esprit.pfe.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Table( name = "User")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	@Column
+	private int id; 
+
+	@Column
+	private String lastname; 
+	
+	@Column
+	private String firstname; 
+	
+	@Column
+	private String email;
+	
+	@Column
+	private String password;
+
+	public User() {
+		super();
+	}
+
+	public User(String lastname, String firstname, String email, String password) {
+		super();
+		this.lastname = lastname;
+		this.firstname = firstname;
+		this.email = email;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+
+}
