1f4d56aec1f729d4ca7bec063250d32c74760c5f
mohamedboufaied <mohamed.boufaied@esprit.tn>
2019-10-23 09:37:59 +0100

    Update ReclamationServiceRemote.java

--- 4twin3-osp-pfe-ejb/src/main/java/tn/esprit/pfe/interfaces/ReclamationServiceRemote.java
+++ 4twin3-osp-pfe-ejb/src/main/java/tn/esprit/pfe/interfaces/ReclamationServiceRemote.java
@@ -2,9 +2,14 @@ package tn.esprit.pfe.interfaces;
 
 import java.util.List;
 
+import javax.ejb.LocalBean;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
 
 import tn.esprit.pfe.entities.Reclamation;
 
+
+@Remote
 public interface ReclamationServiceRemote {
 
 	
@@ -16,7 +21,8 @@ public interface ReclamationServiceRemote {
 	//afficher les soutenances non noté + notifier l'enseignant affecte
 	//afficher les soutenances qui attend un rapporteur
 	//notifier l'etudiant lors de l'ajout d'une note
-
+     //stat note
+      
 	
 	
 }
