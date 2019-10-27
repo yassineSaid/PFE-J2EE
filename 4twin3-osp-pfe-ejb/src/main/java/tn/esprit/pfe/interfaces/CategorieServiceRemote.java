8ab15fca3bfd88408cb1ae48fc421b47ee13e6a6
Adem24 <adem.benzarb@esprit.tn>
2019-10-27 11:53:20 +0100

    Categorie

--- 4twin3-osp-pfe-ejb/src/main/java/tn/esprit/pfe/interfaces/CategorieServiceRemote.java
+++ 4twin3-osp-pfe-ejb/src/main/java/tn/esprit/pfe/interfaces/CategorieServiceRemote.java
@@ -1,10 +1,21 @@
 package tn.esprit.pfe.interfaces;
 import java.util.List;
 
+import javax.ejb.Remote;
+
 import tn.esprit.pfe.entities.Categorie;
 
-
+@Remote
 public interface CategorieServiceRemote {
 	public void addCategorie(Categorie c);
+
+	public void deleteCategorie(int id);
+
+	public List<Categorie> getAllcategorie();
+	
+	public List<String>getCategorielesplusdemandées();
+	
+	public  void addCategoriecommemodule(Categorie c);
+	
 
 }
